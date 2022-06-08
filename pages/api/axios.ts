8069574c4d73c1a41a90b9f692e11f69573786f6
Cem Gurel <gurel.cem374@hotmail.com>
2022-06-08 17:27:05 +0300

    updated axios instance

--- pages/api/axios.ts
+++ pages/api/axios.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const environment = process.env.NODE_ENV;
 axios.defaults.baseURL =
-  environment === "production" ? process.env.URL : "http://localhost:3000/api/";
+  environment === "production"
+    ? "http://codecast.vercel.app/api/"
+    : "http://localhost:3000/api/";
 axios.interceptors.request.use((config) => {
   config.headers.Accept = "application/json";
   config.headers.ContentType = "application/json";
